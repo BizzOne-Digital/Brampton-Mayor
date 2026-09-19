c96e0319edf8f4db4376db5c09d1c15fb984480a
musakiani7 <musa.kiani@unicress.com>
2026-09-19 22:39:00 +0500

    fix

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -78,7 +78,7 @@ export function HeroSection() {
       />
 
       <div
-        className="relative z-10 mx-auto flex min-h-[100dvh] max-w-7xl min-w-0 flex-col justify-start px-4 pb-32 pt-[max(34%,calc(env(safe-area-inset-top,0px)+4.5rem))] sm:px-6 max-lg:pt-[34%] lg:justify-center lg:px-8 lg:pb-24 lg:pt-28"
+        className="relative z-10 mx-auto flex min-h-[100dvh] max-w-7xl min-w-0 flex-col justify-start px-4 pb-32 pt-[max(46%,calc(env(safe-area-inset-top,0px)+4.5rem))] sm:px-6 max-lg:pt-[46%] lg:justify-center lg:px-8 lg:pb-24 lg:pt-28"
       >
         <div className="flex w-full min-w-0 max-w-2xl flex-col lg:max-w-3xl">
           <motion.p
